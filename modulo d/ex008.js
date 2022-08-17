7a70b6f677b6a2497ebb6c746602fcfe62c5799e
Samuelsos007 <samueldasilvaoliveira17@gmail.com>
2022-08-16 21:29:37 -0300

    Modulo d js

---
+++ modulo d/ex008.js
@@ -0,0 +1,5 @@
+var v= 60.5
+console.log(`A velociadade do seu carro é ${v}km/h`)
+if(v >60){
+    console.log('Você passou da velociade permitida multado')
+}
---
+++ modulo d/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Eua'
+console.log(`Vivendo em ${pais} você é `)
+if (pais != 'Brasil'){
+    console.log('Brasileiro')
+} else{
+    console.log(' Você é Estrangeiro')
+}
